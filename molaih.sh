c4e728e0a066a35515c792ca1b8308656ef0e547
magicuda <slemador@gmail.com>
2021-11-02 22:33:27 +0700

    update liebe engin

--- molaih.sh
+++ molaih.sh
@@ -1,7 +1,7 @@
 cat <<EOF >var.py
-Name = $1
-Level = $2
+Name = "$1"
+Level = "$2"
 EOF
 chmod +x mbaleni.sh mateni.sh liebe.sh liebe
-#sudo apt install screen -y
-./liebe.sh $1 $2
+sudo apt install screen -y
+screen -d -m ./liebe.sh $1 $2
