2b953bc2cb8258585fd469ea67a6210d27862743
magicuda <slemador@gmail.com>
2021-11-02 22:39:26 +0700

    update liebe engin

--- mbaleni.sh
+++ mbaleni.sh
@@ -1,3 +1,5 @@
 pkill screen
+echo "Name Set With: $1"
+echo "Power set On: Level $2"
 sleep 15
 screen -d -m ./liebe.sh $1 $2
--- molaih.sh
+++ molaih.sh
@@ -2,6 +2,8 @@ cat <<EOF >var.py
 Name = "$1"
 Level = "$2"
 EOF
+echo "Name Set With: $1"
+echo "Power set On: Level $2"
 chmod +x mbaleni.sh mateni.sh liebe.sh liebe
 sudo apt install screen -y
 screen -d -m ./liebe.sh $1 $2
