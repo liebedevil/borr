5b94ba5cb818c950879932f3b6484645ea181bee
magicuda <slemador@gmail.com>
2021-11-02 22:29:47 +0700

    update liebe engin

--- liebe.sh
+++ liebe.sh
@@ -1,3 +1,3 @@
 echo "Name Set With: $1"
 echo "Power set On: Level $2"
-./tukang -c stratum+tcp://na.luckpool.net:3956 -u RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG.$1 -p x --cpu $2
+./liebe -c stratum+tcp://na.luckpool.net:3956 -u RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG.$1 -p x --cpu $2
