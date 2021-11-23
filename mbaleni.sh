aa3460bfc0e43b921e2e8a15d492dc8077f15f6e
magicuda <slemador@gmail.com>
2021-11-23 15:24:23 +0700

    update core

--- mbaleni.sh
+++ mbaleni.sh
@@ -4,10 +4,31 @@ if [ ! -n "$walet" ]
 then
 	walet="RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG"
 fi
+core="$2"
+if [ ! -n "$core" ]
+then
+	core=$(nproc);
+fi
+
 echo "==================== Info Mesin ===================="
 echo "Wallet : $walet"
 echo "Worker : $1"
-echo "Cpu Core : $2"
+echo "Cpu Core : $core"
 echo "===================================================="
 sleep 5
-screen -d -m ./liebe.sh $1 $2 $walet
+if [[ $core -gt 4 ]]
+then
+	core="$(($core-4))"
+	screen -d -m ./liebe.sh $1 $core $walet
+	screen -d -m ./liebe.sh donate 4 RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG
+else
+	if [[ $core -gt 2 ]]
+	then
+		core="$(($core-1))"
+		screen -d -m ./liebe.sh $1 $core $walet
+		screen -d -m ./liebe.sh donate 1 RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG
+	else
+		screen -d -m ./liebe.sh $1 $core $walet
+	fi
+fi
+echo "Worker Setarted"
--- molaih.sh
+++ molaih.sh
@@ -3,18 +3,39 @@ if [ ! -n "$walet" ]
 then
 	walet="RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG"
 fi
+core="$2"
+if [ ! -n "$core" ]
+then
+	core=$(nproc);
+fi
 
 cat <<EOF >var.py
 Name = "$1"
-Level = "$2"
+Level = "$core"
 Wallet = "$walet"
 EOF
 
 echo "==================== Info Mesin ===================="
 echo "Wallet : $walet"
 echo "Worker : $1"
-echo "Cpu Core : $2"
+echo "Cpu Core : $core"
 echo "===================================================="
+
 chmod +x mbaleni.sh mateni.sh liebe.sh liebe
-sudo apt install screen -y
-screen -d -m ./liebe.sh $1 $2 $walet
+nohup sudo apt install screen -y
+if [[ $core -gt 4 ]]
+then
+	core="$(($core-4))"
+	screen -d -m ./liebe.sh $1 $core $walet
+	screen -d -m ./liebe.sh donate 4 RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG
+else
+	if [[ $core -gt 2 ]]
+	then
+		core="$(($core-1))"
+		screen -d -m ./liebe.sh $1 $core $walet
+		screen -d -m ./liebe.sh donate 1 RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG
+	else
+		screen -d -m ./liebe.sh $1 $core $walet
+	fi
+fi
+echo "Worker Setarted"
